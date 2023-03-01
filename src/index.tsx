352ed431911497170fb1d1276651910a8c054528
yokoish <chmod700yokoish@gmail.com>
2023-03-01 15:53:16 +0900

    eslintとprettierのインストールと適用

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Header from './components/Header'
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
